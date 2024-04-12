db73a241af3ff6a245fe18b94ee28c4b75cb0acd
Rakshith <rakshithvishnu5614@gmail.com>
2024-04-12 21:55:20 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     //object destructuring
@@ -28,6 +29,41 @@ export const signup = async (req, res, next) => {
     //create a response with json
     res.json('Sign Up successfull')
         
+    } catch (error) {
+        next(error)
+    }
+}
+
+//creating a sign in component
+export const signin = async (req, res, next) => {
+    //getting username and password from ui
+    const { email, password } = req.body
+
+    //setting an error depending on certain conditions
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    //validating user
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            next(errorHandler(404, 'User not found'))
+        }
+        //comparing existing password with password if user is valid
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        //if password not valid create an error
+        if(!validPassword) {
+            return next(errorHandler(400, 'Invalid password'))
+        }
+
+        //if both are correct create a token
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY )
+        //separating password
+        const { password: pass, ...rest} = validUser._doc
+        //adding token to cookies
+        res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest)
     } catch (error) {
         next(error)
     }
